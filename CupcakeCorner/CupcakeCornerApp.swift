fcec6b0c21f4a0d0b28e44b552f98dddddfa6927
choijun0 <cjy00050005@gmail.com>
2022-12-06 17:57:48 +0900

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 최준영 on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
